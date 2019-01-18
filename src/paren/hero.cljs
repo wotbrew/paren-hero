615b1c283fac26fec5f53134033dde8ed29711f8
Dan Stone <danielstone@riverford.co.uk>
2019-01-18 13:15:59 +0000

    stuff

--- src/paren/hero.cljs
+++ src/paren/hero.cljs
@@ -1,6 +1,8 @@
 (ns ^:figwheel-hooks paren.hero
   (:require
    [goog.dom :as gdom]
+   [goog.events :as gevents]
+   [goog.events.KeyHandler]
    [oops.core :refer [ocall oget oset!]]))
 
 ;; define your app data so that it doesn't get over-written on reload
@@ -9,10 +11,12 @@
    :delta 0
    :screen-size {:cw 0
                  :ch 0}
+   :caret {:x 0
+           :y 0}
    :forms [{:progress 0.2
             :slot 0
             :bits [{:text "("}
-                   {:text "+ 1 2"
+                   {:text " + 1 2 "
                     :hot? true}
                    {:text ")"}
                    {:text " 3"}]
@@ -30,64 +34,115 @@
 (def cx
   (ocall canvas "getContext" "2d"))
 
+(defn puts
+  [s]
+  (let [{:keys [size]} @game-state
+        {:keys [cw ch]} size]
+    (oset! cx "fillStyle" "#FFFFFF")
+    (oset! cx "font"  "18px Menlo")
+    (ocall cx "fillText" (str s) (/ cw 2) (/ ch 2))))
+
 (defn get-size
   []
   (let [bounding-rect (ocall canvas "getBoundingClientRect")]
     {:cw (oget bounding-rect "width")
      :ch (oget bounding-rect "height")}))
 
-(defn update-form
-  [form]
-  (oset! cx "font"  "80px Menlo")
-  (let [{:keys [bits]} form
-        {:keys [bits x]} (reduce
-                           (fn [acc bit]
-                             (let [{:keys [bits x]} acc
-                                   {:keys [text]} bit
-                                   w (oget (ocall cx "measureText" text) "width")
-                                   bit (assoc bit :w w :x x)]
-                               {:x (+ w x)
-                                :bits (conj bits bit)}))
-                           {:bits []
-                            :x 0}
-                           bits)]
-    (assoc form :bits bits)))
-
-(defn draw-bit [bit x y]
-  (ocall cx "fillText" (:text bit) (+ x (:x bit)) y))
-
-(defn draw-form
-  [form]
-  (let [{:keys [progress slot bits]} form
-        {:keys [size]} @game-state
-        {:keys [cw ch]} size
-        x (- cw (* progress cw))
-        y (+ 100 (* slot (/ ch 10)))]
-    (oset! cx "fillStyle" "#FFFFFF")
-    (oset! cx "font"  "80px Menlo")
-    (run! (fn [bit] (draw-bit bit x y)) bits)))
-
-(defn draw-caret
+(defn update-caret
   [game-state]
   (let [{:keys [size]} game-state
         {:keys [cw ch]} size]
+    (assoc game-state :caret {:x (* cw 0.25)
+                              :y 0
+                              :w 2
+                              :h ch})))
+
+
+(defn intersects?
+  [bit caret]
+  (let [{cx :x cw :w} caret
+        {bx :x bw :w} bit]
+    (and (<= bx cx)
+         (<= (+ cx cw) (+ bx bw)))))
+
+(defn update-form
+  [form]
+  (oset! cx "font"  "80px Menlo")
+  (let [{:keys [bits
+                progress
+                slot]} form
+        {:keys [size
+                caret]} @game-state
+        {:keys [cw ch]} size
+        x (- cw (* progress cw))
+        y (+ 100 (* slot (/ ch 10)))
+        form (assoc form :x x,
+                         :y y,
+                         :active? false
+                         :color "#FFFFFF")]
+    (loop [i 0
+           xacc x
+           form form
+           bits bits]
+      (if-not (< i (count bits))
+        (assoc form :bits bits)
+        (let [bit (nth bits i)
+              {:keys [text]} bit
+              w (oget (ocall cx "measureText" text) "width")
+              bit (assoc bit :y y :w w :x xacc)
+              active? (and (:hot? bit)
+                           (intersects? bit caret))
+              form (if active?
+                     (assoc form :color "#FF0000")
+                     form)
+              bit (assoc bit :active? active?)]
+          (recur (inc i)
+                 (+ xacc w)
+                 (assoc form :active? (or (:active? form) active?))
+                 (assoc bits i bit)))))))
+
+(defn key-up
+  [game-state key-code]
+  (update game-state :keys dissoc key-code))
+
+(defn key-down
+  [game-state key-code]
+  (assoc-in game-state [:keys key-code] (:elapsed-time game-state)))
+
+(defn draw-bit [bit]
+  (let [{:keys [text x y]} bit]
+    (ocall cx "fillText" text x y )))
+
+(defn draw-form
+  [form]
+  (let [{:keys [progress slot color bits]} form
+        {:keys [size]} @game-state
+        {:keys [cw ch]} size]
+    (oset! cx "fillStyle" color)
+    (oset! cx "font"  "80px Menlo")
+    (run! draw-bit bits)))
+
+(defn draw-caret
+  [game-state]
+  (let [{:keys [caret]} game-state
+        {:keys [x y w h]} caret]
     (oset! cx "fillStyle" "#FFFFFF")
-    (ocall cx "fillRect" (* cw 0.25) 0 2 ch)))
+    (ocall cx "fillRect" x y w h)))
 
 (defn draw [game-state]
   (let [{:keys [last-time forms size]} game-state
         {:keys [cw ch]} size]
-    (ocall cx "clearRect" 0 0 cw ch)
-    (oset! cx "fillStyle" "#333333")
-    (ocall cx "fillRect" 0 0 cw ch)
     (draw-caret game-state)
-    (run! draw-form forms)))
+    (run! draw-form forms)
+
+    (puts (:keys game-state))
+    ))
 
 (defn update-forms [game-state]
   (let [{:keys [delta]} game-state]
     (update game-state :forms
             (fn [forms]
-              (map (fn [f]
+              (mapv (fn [f]
                      (let [{:keys [progress velocity]} f
                            new-progress (+ progress (* delta velocity))]
                        (-> (assoc f :progress new-progress)
@@ -95,26 +150,72 @@
 
 (defonce last-time (atom nil))
 
+(defn envstate
+  []
+  (let [current-time (.getTime (js/Date.))
+        size (get-size)
+        t (or @last-time current-time)
+        delta (/ (- current-time t) 1000)]
+    {:size size
+     :current-time current-time
+     :delta delta}))
+
+(defn update-game
+  [game-state envstate]
+  (let [{:keys [elapsed-time]} game-state
+        {:keys [delta size]} envstate]
+    (-> (assoc
+          game-state
+          :size size
+          :elapsed-time (+ elapsed-time delta)
+          :delta delta)
+        (update-caret)
+        (update-forms))))
+
+(defn clear-screen
+  [cw ch]
+  (ocall cx "clearRect" 0 0 cw ch)
+  (oset! cx "fillStyle" "#333333")
+  (ocall cx "fillRect" 0 0 cw ch))
+
 (defn game-loop []
   (js/requestAnimationFrame game-loop)
-  (let [current-time (.getTime (js/Date.))
-        {:keys [cw ch] :as size} (get-size)
-        new-state (swap! game-state (fn [game-state]
-                                      (update-forms
-                                        (let [{:keys [elapsed-time]} game-state
-                                              t (or @last-time current-time)
-                                              delta (/ (- current-time t) 1000)]
-                                          (assoc
-                                            game-state
-                                            :size size
-                                            :elapsed-time (+ elapsed-time delta)
-                                            :delta delta)))))]
+
+  (let [{:keys [current-time
+                size]
+         :as envstate} (envstate)
+        {:keys [cw ch]} size]
     (oset! canvas "width" cw)
     (oset! canvas "height" ch)
-    (draw new-state)
+    (clear-screen cw ch)
+    (try
+      (let [new-state (swap! game-state update-game envstate)]
+        (draw new-state))
+      (catch :default e
+        (puts e)))
+
     (reset! last-time current-time)))
 
-(defonce _ (game-loop))
+(defn key-up-handler
+  [event]
+  (let [key-code (oget event "keyCode")]
+    (swap! game-state key-up key-code)))
+
+(defn key-down-handler
+  [event]
+  (let [key-code (oget event "keyCode")]
+    (swap! game-state key-down key-code)))
+
+(defn init-input
+  []
+  (oset! js/document "onkeydown" key-down-handler)
+  (oset! js/document "onkeyup" key-up-handler))
+
+(defonce _ (do
+             (init-input)
+             (game-loop)))
+
+
 ;; specify reload hook with ^;after-load metadata
 (defn ^:after-load on-reload []
   ;; optionally touch your app-state to force rerendering depending on
